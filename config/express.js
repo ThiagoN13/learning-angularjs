9697ca00ef9ffc7f72e12aa4262316e71708cb81
Diego Leal <leal.banks@gmail.com>
2017-05-02 22:12:24 -0300

    Adicionado configurações de porta e do mongoose

--- config/express.js
+++ config/express.js
@@ -1,25 +1,32 @@
-var express = require('express'),
-    load = require('express-load'),
-    bodyParser = require('body-parser');
+var express = require( 'express' ),
+  load = require( 'express-load' ),
+  bodyParser = require( 'body-parser' ),
+  path = require( 'path' ),
+  mongoose = require( './mongoose' );
 
-module.exports = function() {
+var PORT = 3000,
+  ROOT_PATH = path.parse( __dirname ).dir;
+
+module.exports = (function() {
   var app = express();
 
   // variaveis de ambiente
+  app.set( 'port', PORT );
+  app.set( 'views', ROOT_PATH + '/app/partials' );
+  app.set( 'view engine', 'html' );
 
-  app.set('port', 3000);
-  app.set('views', './partials');
-  app.set('view engine', 'html');
-
-
-  // middleares
-  app.use(express.static('./public'));
+  // middlewares
+  app.use(express.static( ROOT_PATH + '/app' ));
   app.use(bodyParser.urlencoded({extended: true}));
-  app.use(bodyParser.json())
+  app.use(bodyParser.json());
 
-  load('controllers', {cwd: 'app'})
-    .then('routers')
-    .into(app);
+  //start mongoose driver
+  mongoose( 'mongodb://localhost/agenda' );
+
+  load( 'models', {cwd: 'api'} )
+    .then( 'controllers' )
+    .then( 'routers' )
+    .into( app );
 
   return app;
-};
+})();
