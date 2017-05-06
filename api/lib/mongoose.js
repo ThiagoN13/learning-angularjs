2b07b49d4e153de1052186ee937b30d43c50fb37
thiago_n13 <thiago.nogueira187@gmail.com>
2017-05-06 00:04:06 -0300

    Adicionado funções get, remove, query, logicalRemove

--- api/lib/mongoose.js
+++ api/lib/mongoose.js
@@ -1,6 +1,7 @@
 var q = require( 'q' ),
   mongoose = require( 'mongoose' ),
   merge = require( 'merge' ),
+  that = require( './that/that' ),
   ObjectId = mongoose.Types.ObjectId;
 
 module.exports = function( Schema ) {
@@ -8,6 +9,12 @@ module.exports = function( Schema ) {
   function _save( doc ) {
     var dfd = q.defer(),
       schema;
+
+    if ( !that.isObject( doc ) ) {
+      dfd.reject( { err: 'Validation error: Value sent is not of object type'} );
+    } else if ( !that.isEmpty( doc ) ) {
+      dfd.reject( { err: 'Validation error: Value sent is empty'} );
+    }
 
     if ( !doc._id ) {
       doc._id = new ObjectId();
@@ -39,8 +46,82 @@ module.exports = function( Schema ) {
 
   }
 
+  function _query( ) {
+    var dfd = q.defer();
+
+    Schema.find( {}, function( err, res ) {
+      if ( err ) {
+        dfd.reject( err );
+      } else {
+        dfd.resolve( res );
+      }
+    });
+
+    return dfd.promise;
+  }
+
+  function _get( doc ) {
+    var dfd = q.defer();
+    doc._id = doc._id ;
+
+    if ( !that.isObject( doc ) ) {
+      dfd.reject( { err: 'Validation error: Value sent is not of object type'} );
+    }
+
+    Schema.findOne( { _id: doc._id, active: true }, function( err, res ) {
+      if ( err ) {
+        dfd.reject( err );
+      } else {
+        dfd.resolve( res );
+      }
+    });
+
+    return dfd.promise;
+  }
+
+
+  function _logicalRemove( doc ) {
+    var dfd = q.defer();
+
+    if ( !that.isObject( doc ) ) {
+      dfd.reject( { err: 'Validation error: Value sent is not of object type'} );
+    }
+
+    Schema.update( { _id: doc._id, active: true }, { $set: { active: false } }, function( err, res) {
+      if ( err ) {
+        dfd.reject( err );
+      } else {
+        dfd.resolve( res );
+      }
+    });
+
+    return dfd.promise;
+  }
+
+  function _remove( doc ) {
+    var dfd = q.defer();
+
+    if ( !that.isObject( doc ) ) {
+      dfd.reject( { err: 'Validation error: Value sent is not of object type'} );
+    }
+
+    Schema.remove( { _id: doc._id, active: true }, function( err, res ) {
+      if ( err ) {
+        dfd.reject( err );
+      } else {
+        dfd.resolve( res );
+      }
+    });
+
+    return dfd.promise;
+  }
+
   return {
-    save: _save
+    save: _save,
+    query: _query,
+    get: _get,
+    logicalRemove: _logicalRemove,
+    remove: _remove
   };
 
 };
